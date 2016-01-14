3887866a1c8f036b89539464615567967e99885d
YS <jyys98@gmail.com>
2016-01-14 17:16:11 +0800

    新闻的增删改查和页面制作完成

--- DeviceCloud/Areas/Admin/Controllers/NewsController.cs
+++ DeviceCloud/Areas/Admin/Controllers/NewsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using DeviceCloud.Models;
 
@@ -44,19 +46,22 @@ namespace DeviceCloud.Areas.Admin.Controllers
         // POST: Admin/News/Create
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
+        [System.Web.Mvc.HttpPost]
         [ValidateInput(false)]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Title,Introduction,CorverUrl,Content,CreateTime")] News news)
         {
-            if (ModelState.IsValid)
+            try
             {
                 db.News.Add(news);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(news);
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message+ex.StackTrace);
+                return View(news);
+            }
         }
 
         // GET: Admin/News/Edit/5
@@ -77,14 +82,14 @@ namespace DeviceCloud.Areas.Admin.Controllers
         // POST: Admin/News/Edit/5
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
+        [System.Web.Mvc.HttpPost]
         [ValidateInput(false)]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Title,Introduction,CorverUrl,Content,CreateTime")] News news)
         {
             if (ModelState.IsValid)
             {
-                news.Content = Server.UrlEncode(Server.HtmlEncode(news.Content));
+                news.Content = Server.UrlEncode(news.Content);
                 db.Entry(news).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -92,25 +97,10 @@ namespace DeviceCloud.Areas.Admin.Controllers
             return View(news);
         }
 
-        // GET: Admin/News/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            News news = db.News.Find(id);
-            if (news == null)
-            {
-                return HttpNotFound();
-            }
-            return View(news);
-        }
-
         // POST: Admin/News/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [System.Web.Mvc.HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete([FromBody]int id)
         {
             News news = db.News.Find(id);
             db.News.Remove(news);
